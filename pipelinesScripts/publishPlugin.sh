c539d59b461514a5213dd7d6efaffc2287ba66bd
eyalbe4 <eyalbenmoshe@jfrog.com>
2022-04-17 16:28:52 +0300

    Fix mis-spelled jfrog-cli command

--- pipelinesScripts/publishPlugin.sh
+++ pipelinesScripts/publishPlugin.sh
@@ -2,7 +2,7 @@
 
 # Publish plugin -build, verify unique version and upload for all architectures.
 publishPlugin () {
-  jf plugins p "$JFROG_CLI_PLUGIN_PLUGIN_NAME" "$JFROG_CLI_PLUGIN_VERSION"
+  jf plugin p "$JFROG_CLI_PLUGIN_PLUGIN_NAME" "$JFROG_CLI_PLUGIN_VERSION"
 }
 
 publishPlugin
