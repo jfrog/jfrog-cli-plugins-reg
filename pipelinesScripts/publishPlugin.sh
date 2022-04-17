94f13f52d48d4799f9d403253b5c93af8d3e328e
Eyal Ben Moshe <eyalbenmoshe@jfrog.com>
2022-04-17 18:12:05 +0300

    Bug fix - Add the missing JFROG_CLI_PLUGINS_SERVER env var (#106)

--- pipelinesScripts/publishPlugin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Publish plugin -build, verify unique version and upload for all architectures.
-publishPlugin () {
-  jf plugin p "$JFROG_CLI_PLUGIN_PLUGIN_NAME" "$JFROG_CLI_PLUGIN_VERSION"
-}
-
-publishPlugin
